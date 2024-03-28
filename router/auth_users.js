e7188e4ccdf0e3450d5df9adba08d085f78f0cf5
Sudhan Poudel <sudhanpoudel2@gmail.com>
2024-03-28 06:26:40 -0400

    first

---
+++ router/auth_users.js
@@ -0,0 +1,89 @@
+const express = require('express');
+const jwt = require('jsonwebtoken');
+let books = require("./booksdb.js");
+const regd_users = express.Router();
+
+let users = [];
+
+
+
+const isValid = (username)=>{ //returns boolean
+//write code to check is the username is valid
+}
+
+const authenticatedUser = (username,password)=>{ //returns boolean
+//write code to check if username and password match the one we have in records.
+let validusers = users.filter((user)=>{
+    return (user.username === username && user.password === password)
+  });
+  if(validusers.length > 0){
+    return true;
+  } else {
+    return false;
+  }
+
+}
+
+//only registered users can login
+regd_users.post("/login", (req,res) => {
+  //Write your code here
+  const username = req.body.username;
+  const password = req.body.password;
+
+  if (!username || !password) {
+      return res.status(404).json({message: "Error logging in"});
+  }
+
+  if (authenticatedUser(username,password)) {
+    let accessToken = jwt.sign({
+      data: password
+    }, 'access', { expiresIn: 60 * 60 });
+
+    req.session.authorization = {
+      accessToken,username
+  }
+  return res.status(200).send("User successfully logged in");
+  } else {
+    return res.status(208).json({message: "Invalid Login. Check username and password"});
+  }
+//   return res.status(300).json({message: "Yet to be implemented"});
+});
+
+// Add a book review
+regd_users.put("/auth/review/:isbn", (req, res) => {
+  //Write your code here
+  const isbn=req.params.isbn;
+  const key = req.params["review"]
+  const value=req.params.value;
+  console.log("value is "+key);
+  let booksbytitle;
+  for(i in books){
+    if(i===isbn)
+    {
+         booksbytitle =books[i];
+         booksbytitle["reviews"]=value;
+    }
+ }
+
+  res.send("Review for the book with ISBN "+isbn+" has been added or updated");
+  return res.status(300).json({message: "Yet to be implemented"});
+});
+
+regd_users.delete("/auth/review/:isbn", (req, res) => {
+    const isbn=req.params.isbn;
+    let booksbytitle;
+    for(i in books){
+      if(i===isbn)
+      {
+           //booksbytitle =books[i];
+           //booksbytitle["reviews"]=value;
+           delete books[i];
+      }
+   }
+
+    res.send("Review for ISBN "+isbn+" posted by user " + req.body.username+" deleted");
+});
+
+module.exports.authenticated = regd_users;
+module.exports.isValid = isValid;
+module.exports.users = users;
